339c90b88cc6d169bfa552e976afb7815027aae4
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2023-11-30 09:25:50 +0700

    update code

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,11 +3,11 @@
         <button class="header-toggler px-md-0 me-md-3" type="button"
             onclick="coreui.Sidebar.getInstance(document.querySelector('#sidebar')).toggle()">
             <svg class="icon icon-lg">
-                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-menu"></use>
+                <use xlink:href="{{ asset('admin/vendors/@coreui/icons/svg/free.svg#cil-menu') }}"></use>
             </svg>
         </button><a class="header-brand d-md-none" href="#">
             <svg width="118" height="46" alt="CoreUI Logo">
-                <use xlink:href="assets/brand/coreui.svg#full"></use>
+                <use xlink:href="{{ asset('admin/assets/brand/coreui.svg#full') }}"></use>
             </svg></a>
         <ul class="header-nav d-none d-md-flex">
             <li class="nav-item"><a class="nav-link" href="#">Dashboard</a></li>

ec19952636fff64067193e05f9cb86bef054e1af
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2023-11-30 09:22:46 +0700

    update code

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard</title>
-</head>
-<body>
-    <h1> welcome you ! </h1>
-</body>
-</html>
+@extends('admin-layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-12 col-md-12">
+            <h5>Dashboard page</h5>
+        </div>
+    </div>
+@endsection
